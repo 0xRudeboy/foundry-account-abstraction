f0e1a252f9899cc1182ac812868dda0999eca376
0xRudeboy <rudeboyweb3@gmail.com>
2025-01-03 14:46:32 +0400

    add validateUserOp function to satisfy IAccount interface

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.24;
 
+import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+
 // Learn more about how AA smart contracts should look like: https://eips.ethereum.org/EIPS/eip-4337
 
-contract MinimalAccount {}
+contract MinimalAccount is IAccount {
+    function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
+        external
+        returns (uint256 validationData)
+    {}
+}
