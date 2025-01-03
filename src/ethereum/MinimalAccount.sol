e1f0514bdad1c96e0c6b342554a172f306411361
0xRudeboy <rudeboyweb3@gmail.com>
2025-01-03 10:53:44 +0400

    update README file and initiate MinimalAccount contract instance

---
+++ src/ethereum/MinimalAccount.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
+
+// Learn more about how AA smart contracts should look like: https://eips.ethereum.org/EIPS/eip-4337
+
+contract MinimalAccount {}
