468a509d96496d23d12ae3770bc48118fa7cc004
0xRudeboy <rudeboyweb3@gmail.com>
2025-01-13 10:25:32 +0400

    create signature validation and repayment of gas functions

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -3,12 +3,48 @@ pragma solidity ^0.8.24;
 
 import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
 import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {SIG_VALIDATION_FAILED, SIG_VALIDATION_SUCCESS} from "lib/account-abstraction/contracts/core/Helpers.sol";
 
 // Learn more about how AA smart contracts should look like: https://eips.ethereum.org/EIPS/eip-4337
 
-contract MinimalAccount is IAccount {
+contract MinimalAccount is IAccount, Ownable {
+    constructor() Ownable(msg.sender) {}
+
+    // A signature is valid if its the minimal account contract owner (we can get creative and add more logic but for this example we'll keep it simple)
     function validateUserOp(PackedUserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
         external
         returns (uint256 validationData)
-    {}
+    {
+        validationData = _validateSignature(userOp, userOpHash);
+        // _validateNonce()
+        // _payPrefund(missingAccountFunds);
+    }
+
+    // EIP-191 version of the signed hash
+    // Here essentially we could add any logic as long as we return SIG_VALIDATION_SUCCESS or SIG_VALIDATION_FAILED for a valid or invalid signature respectively
+    function _validateSignature(PackedUserOperation calldata userOp, bytes32 userOpHash)
+        internal
+        view
+        returns (uint256)
+    {
+        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+        address signer = ECDSA.recover(ethSignedMessageHash, userOp.signature);
+
+        if (signer != owner()) {
+            return SIG_VALIDATION_FAILED;
+        }
+
+        return SIG_VALIDATION_SUCCESS;
+    }
+
+    // Paying back the entry point contract that initially paid the gas to submit our transaction
+    function _payPrefund(uint256 missingAccountFunds) internal {
+        if (missingAccountFunds != 0) {
+            (bool success,) = payable(msg.sender).call{value: missingAccountFunds, gas: type(uint256).max}("");
+            (success);
+        }
+    }
 }
