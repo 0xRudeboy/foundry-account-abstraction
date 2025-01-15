0e47828f2b0f59090d23f6ff6fd42ee23663bf1b
0xRudeboy <rudeboyweb3@gmail.com>
2025-01-15 10:21:57 +0400

    install foundry-era-contracts and import the main function needed for the ZkMinimalAccount

---
+++ src/zksync/ZkMinimalAccount.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import {IAccount} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/IAccount.sol";
+import {Transaction} from
+    "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
+
+contract ZkMinimalAccount is IAccount {
+    /**
+     * Phase 1 Validation
+     * 1.  The user sends the transaciton to the "zkSync API client" (sort of a "light node")
+     * 2. The zkSync API client checks to see if the nonce is unique by querying the NonceHolder system contract
+     *
+     * Phase 2 Execution
+     */
+    function validateTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction memory _transaction)
+        external
+        payable
+        returns (bytes4 magic)
+    {}
+
+    function executeTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction memory _transaction)
+        external
+        payable
+    {}
+
+    function executeTransactionFromOutside(Transaction memory _transaction) external payable {}
+
+    function payForTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction memory _transaction)
+        external
+        payable
+    {}
+
+    function prepareForPaymaster(bytes32 _txHash, bytes32 _possibleSignedHash, Transaction memory _transaction)
+        external
+        payable
+    {}
+}
